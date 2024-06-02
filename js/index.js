c00643fb535f2bb7291d3445e5e4f87c85694445
Oltinbek2010 <oltinbek04@gmail.com>
2024-06-02 14:25:59 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// let num = +prompt (`Uch xonali son kiriting`);
+// let yuzlar = Math.trunc(num / 100);
+// let onlar = Math.trunc(num / 10) % 10;
+// let birliklar = num % 10;
+// let res = birliklar * 100 + onlar * 10 + yuzlar
+// console.log(res);
+
+let age = +prompt(son);
+age++;
