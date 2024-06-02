70cacc0878ba501de41db9367d980632900ac196
Oltinbek2010 <oltinbek04@gmail.com>
2024-06-02 14:54:30 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,84 @@
-// let num = +prompt (`Uch xonali son kiriting`);
-// let yuzlar = Math.trunc(num / 100);
-// let onlar = Math.trunc(num / 10) % 10;
-// let birliklar = num % 10;
-// let res = birliklar * 100 + onlar * 10 + yuzlar
-// console.log(res);
+ /**
+ * 9-masala
+ * let n = +prompt ("Uch xonali son kiriting");
+ * let yuzlar = Math.trunc(n / 100);
+ * console.log(yuzlar);
+ */
 
-let age = +prompt(son);
-age++;
+ /**
+  * 10-masala
+  * let n = +prompt ("Uch xonali son kiriting");
+  * let onlar = Math.trunc(n / 10) % 10;
+  * let birlar = n % 10;
+  * console.log(onlar, birlar);
+  */
+
+/**
+ * 11-masala
+ * let n = +prompt ("Uch xonali son kiriting");
+ * let yuzlar = Math.trunc(n / 100);
+ * let onlar = Math.trunc(n / 10) % 10;
+ * let birlar = n % 10;
+ * let result = yuzlar + onlar + birlar;
+ * console.log(result);
+ */
+
+/**
+ * 12-masala
+ * let n = +prompt ("Uch xonali son kiriting");
+ * let yuzlar = Math.trunc(n / 100);
+ * let onlar = Math.trunc(n / 10) % 10;
+ * let birlar = n % 10;
+ * let result = birlar * 100 + onlar * 10 + yuzlar;
+ * console.log(result);
+ */
+
+/**
+ * 13-masala
+ * let n = +prompt ("Uch xonali son kiriting");
+ * let yuzlar = Math.trunc(n / 100);
+ * let onlar = Math.trunc(n / 10) % 10;
+ * let birlar = n % 10;
+ * let result = onlar * 100 + birlar * 10 + yuzlar;
+ * console.log(result);
+ */
+
+/**
+ * 14-masala
+ * let n = +prompt ("Uch xonali son kiriting");
+ * let yuzlar = Math.trunc(n / 100);
+ * let onlar = Math.trunc(n / 10) % 10;
+ * let birlar = n % 10;
+ * let result = birlar * 100 + onlar * 10 + yuzlar;
+ * console.log(result);
+ */
+
+/**
+ * 15-masala
+ * let n = +prompt ("Uch xonali son kiriting");
+ * let yuzlar = Math.trunc(n / 100);
+ * let onlar = Math.trunc(n / 10) % 10;
+ * let birlar = n % 10;
+ * let result = onlar * 100 + yuzlar * 10 + birlar;
+ * console.log(result);
+ */
+
+/**
+ * 16-masala
+ * let n = +prompt ("999dan katta son kiriting")
+ * let yordamchi = n % 1000;
+ * let yuzlar = Math.trunc(yordamchi / 100);
+ * console.log(yuzlar);
+ */
+
+/**
+ * 17-masala
+ * let n = +prompt ("999dan katta son kiriting")
+ * let minglar = (n / 1000);
+ * console.log(minglikar);
+ */
+
+/**
+ * 18-masala
+ * 
+ */
